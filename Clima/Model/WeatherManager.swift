d30b95fc469ff948ffffb2c27dd97a7ef80d1803
Joel <joellacerdaol@gmail.com>
2022-06-23 18:11:55 -0300

    third commit

--- Clima/Model/WeatherManager.swift
+++ Clima/Model/WeatherManager.swift
@@ -17,21 +17,36 @@ struct WeatherManager {
     }
     
     func performRequest(urlString: String) {
+        // Create URL
         if let url = URL(string: urlString) {
+            // Create URLSession
             let session = URLSession(configuration: .default)
-            let task = session.dataTask(with: url, completionHandler: handle(data: response: error: ))
+            // Give the session a task
+            let task = session.dataTask(with: url) { data, response, error in
+                if error != nil {
+                    print(error!)
+                    return
+                }
+                if let safeData = data {
+                    parseJSON(weatherData: safeData)
+                }
+            }
+            // Start the task
             task.resume()
         }
     }
     
-    func handle(data: Data?, response: URLResponse?, error: Error?){
-        if error != nil {
-            print(error!)
-            return
-        }
-        if let safeData = data {
-            let dataString = String(data: safeData, encoding: .utf8)
-            print(dataString!)
+    func parseJSON (weatherData: Data) {
+        let decoder = JSONDecoder()
+        do {
+            let decodedData = try decoder.decode(WeatherData.self, from: weatherData)
+            let id = decodedData.weather[0].id
+            let temp = decodedData.main.temp
+            let name = decodedData.name
+            var weather = WeatherModel(conditionID: id, cityName: name, temperature: temp)
+            print(weather.temperatureString)
+        } catch {
+            print(error)
         }
     }
 }
